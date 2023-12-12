017fd00994b1b54c6f092c7f85d1bd95bd55edec
Peter Huang <peterhuang1113@gmail.com>
2023-12-11 19:04:51 -0500

    try this

--- scripts/checkPrTemplate.js
+++ scripts/checkPrTemplate.js
@@ -1,17 +1,26 @@
 checkPrTemplate();
+
 function checkString(s) {
   console.log(
     "------------------------------------------------------------------"
   );
   console.log("Input string: ".concat(s));
-  var regex = new RegExp(/(- \[[ ]\].+)/g);
-  var uncompletedTasks = s.match(regex);
-  console.log("uncompletedTasks: ".concat(uncompletedTasks.join("\n")));
   console.log(
     "------------------------------------------------------------------"
   );
-  process.exit(uncompletedTasks ? 1 : 0);
+  var regex = new RegExp(/(- \[[ ]\].+)/g);
+  var uncompletedTasks = s.match(regex);
+  if (uncompletedTasks) {
+    console.log("uncompletedTasks: \n".concat(uncompletedTasks.join("\n")));
+    console.log(
+      "------------------------------------------------------------------"
+    );
+    process.exit(1);
+  } else {
+    process.exit(0);
+  }
 }
+
 function checkPrTemplate() {
   var argv = process.argv[2];
   if (!argv) {
