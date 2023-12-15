7a150cd00c074f4ddd5b921d8b165d4d861b9a88
Peter Huang <peterhuang1113@gmail.com>
2023-12-15 08:10:58 -0500

    set code coverage

--- e2e-app/global-teardown.ts
+++ e2e-app/global-teardown.ts
@@ -2,23 +2,25 @@ import { join } from 'path';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import NYC from 'nyc';
+import { promises as fs } from 'fs';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const coverageThresholds = require('../e2e-app/coverage-thresholds.json');
 
 async function globalTeardown() {
-  const nycInstance = new NYC({
-    cwd: join(__dirname, '.'),
-    reportDir: `coverage-e2e`,
-    reporter: ['lcov', 'json', 'text-summary'],
-  });
-  nycInstance.report();
   try {
-    nycInstance.checkCoverage(coverageThresholds);
+    const nycOutput = join(__dirname, '.', '.nyc_output');
+    const nycInstance = new NYC({
+      cwd: join(__dirname, '.'),
+      reportDir: `coverage-e2e`,
+      reporter: ['lcov', 'json', 'text-summary'],
+    });
+    await nycInstance.checkCoverage(coverageThresholds);
+    await nycInstance.report();
+    await fs.rm(nycOutput, { recursive: true, force: true });
   } catch (e) {
+    // NYC doesn't throw error when coverage is not met. bug
     console.error('Insufficient playwright code coverage!');
-  } finally {
-    nycInstance.cleanup();
   }
 }
 
