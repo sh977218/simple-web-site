afa5299dea47f9666314ae842b75ad4cbe9735d6
Peter Huang <peterhuang1113@gmail.com>
2023-12-16 14:11:52 -0500

    fix(repo): fix tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./e2e-app/src/**/*.{html,ts}'],
+  content: ['src/**/*.{html,ts}'],
   theme: {
     extend: {
       screens: {
