7d9789aa18afa63dca8905b261b8ba0dd52d0b26
Peter Huang <peterhuang1113@gmail.com>
2024-01-18 09:35:29 -0500

    fix tests

--- test/src/page-title.spec.ts
+++ test/src/page-title.spec.ts
@@ -4,10 +4,6 @@ import test from 'test/fixtures/baseTest';
 test('Check page title', async ({ page }) => {
   await page.goto('/');
   await expect(page).toHaveTitle(`AngularWithPlaywright`);
-  const homeLink = page.locator(`[data-automation-id="home-link"]`);
-  await homeLink.click();
+  await page.locator(`[data-automation-id="home-link"]`).click();
   await expect(page).toHaveTitle(`Home`);
-  await page.locator(`[data-automation-id="helps-link"]`).click();
-  await page.locator(`[data-automation-id="about-link"]`).click();
-  await expect(page).toHaveTitle(`About`);
 });
