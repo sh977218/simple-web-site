261d4ea2613cd82bfa3cd144ce38c6e20c149c48
Peter Huang <peterhuang1113@gmail.com>
2024-01-18 10:03:29 -0500

    feat(test): add test

--- test/src/home-page.spec.ts
+++ test/src/home-page.spec.ts
@@ -4,6 +4,7 @@ import test from 'test/fixtures/baseTest';
 test('Click home link', async ({ page }) => {
   await page.goto('/');
   await page.locator(`[data-automation-id="home-link"]`).click();
+  await expect(page).toHaveTitle(`Home`);
   await expect(
     page.getByText(
       `Some very very very very very very very very very very very very very very long texts.`
