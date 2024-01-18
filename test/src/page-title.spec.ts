1c7ebbe555c912aa6d26f517c073390f436e35be
Peter Huang <peterhuang1113@gmail.com>
2024-01-18 09:58:50 -0500

    feat(playwright): add sharding for playwright tests (#36)

--- test/src/page-title.spec.ts
+++ test/src/page-title.spec.ts
@@ -1,10 +1,9 @@
 import { expect } from '@playwright/test';
 import test from 'test/fixtures/baseTest';
 
-test('Click home link', async ({ page }) => {
+test('Check page title', async ({ page }) => {
   await page.goto('/');
   await expect(page).toHaveTitle(`AngularWithPlaywright`);
-  const homeLink = page.locator(`[data-automation-id="home-link"]`);
-  await homeLink.click();
+  await page.locator(`[data-automation-id="home-link"]`).click();
   await expect(page).toHaveTitle(`Home`);
 });
