9fdab5d62551415768d96876c2139b8e82901c95
Peter Huang <peterhuang1113@gmail.com>
2024-07-20 11:05:12 -0400

    feat(theme): add material theme various

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,46 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
-import { of } from 'rxjs';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatInputModule } from '@angular/material/input';
 import { RouterLink } from '@angular/router';
+import { ColorService } from 'src/app/color-service';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     CommonModule,
+    RouterLink,
     MatButtonModule,
     MatGridListModule,
     MatTabsModule,
     MatIconModule,
     MatButtonToggleModule,
     MatInputModule,
-    RouterLink,
+    MatCheckboxModule,
+    MatChipsModule,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-  welcome_message = 'welcome';
-  flag = false;
-
-  clickMe() {
-    this.welcome_message = 'welcome world!';
-  }
-
-  clickMeAgain() {
-    of({ message: 'hello world' }).subscribe({
-      next: (res) => {
-        this.welcome_message = res.message;
-      },
-      error: () => {
-        this.welcome_message = 'Error loading.';
-      },
-    });
-  }
+  colorService = inject(ColorService);
 }
