7682f751955720672e8dbaa126dd82b21307a036
Peter Huang <peterhuang1113@gmail.com>
2024-12-11 09:36:45 -0500

    feat(repo): add winston logger

--- server/app.js
+++ server/app.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const expressWinston = require('express-winston');
+const logger = require('./logger'); // Import the logger instance
 const cors = require('cors');
 
 const app = express();
 const port = 3000;
 app.use(cors());
+// Use express-winston for request logging
+app.use(
+  expressWinston.logger({
+    winstonInstance: logger,
+    meta: true, // Log metadata about the request
+    msg: 'HTTP {{req.method}} {{req.url}}', // Customize log message
+  })
+);
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
@@ -20,15 +30,33 @@ function mongoClient() {
 }
 
 async function mongoDb() {
-  const client = await mongoClient().connect();
+  const client = await mongoClient()
+    .connect()
+    .catch((err) => {
+      logger.error(err.toString());
+    });
   return client.db('test');
 }
 
 app.get('/api/heroes', async (req, res) => {
+  logger.info('GET /api/heroes route accessed'); // Log an info message
   const HeroesCollection = (await mongoDb()).collection('heroes');
   const heroes = await HeroesCollection.find({}).toArray();
   res.send(heroes);
 });
+
+app.get('/api/error', async (req, res) => {
+  logger.info('GET /api/error route accessed'); // Log an info message
+  logger.error('system error'); // Log an error message
+  res.status(500).send('system error');
+});
+
+// Use express-winston for error logging
+app.use(
+  expressWinston.errorLogger({
+    winstonInstance: logger,
+  })
+);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
