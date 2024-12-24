6180bf06393550780519d9689dbe4d0d012bf189
Peter Huang <peterhuang1113@gmail.com>
2024-12-24 10:31:53 -0500

    done

--- playwright.config.ts
+++ playwright.config.ts
@@ -46,7 +46,7 @@ export default defineConfig({
 
   webServer: [
     {
-      command: 'npm run start:server',
+      command: 'npm run start:server --prefix client',
       port: 3000,
       reuseExistingServer: true,
     },
