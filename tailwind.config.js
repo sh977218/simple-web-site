ef556964665cc7e4b2e136cf87c5429a6634caeb
Peter Huang <peterhuang1113@gmail.com>
2024-12-24 15:51:01 -0500

    revert

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['client/simple-web-site/src/**/*.{html,ts}'],
+  theme: {
+    extend: {
+      screens: {
+        sm: '660px',
+
+        md: '845px',
+
+        lg: '860px',
+
+        xl: '1200px',
+      },
+    },
+  },
+  plugins: [],
+  important: true,
+};
