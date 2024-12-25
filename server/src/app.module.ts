eeaeb15764daaf7960945ddd2de3f0d9312d160a
Peter Huang <peterhuang1113@gmail.com>
2024-12-25 09:38:30 -0500

    feat(repo): using build for playwright (#105)

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,15 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import configuration from 'config/configuration';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { ConfigModule } from '@nestjs/config';
+
 import { HeroesModule } from 'src/heroes/heroes.module';
+import configuration from 'src/config/configuration';
 
 @Module({
   imports: [
     // ConfigModule.forRoot({
     //   load: [configuration],
     // }),
+    ServeStaticModule.forRoot({
+      rootPath: join(
+        __dirname,
+        '.',
+        process.env.COVERAGE ? 'client' : 'client/browser',
+      ),
+    }),
     MongooseModule.forRoot('mongodb://localhost:27017', { dbName: 'test' }),
     HeroesModule,
   ],
