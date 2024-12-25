94b3d53803c25918812ceab55ca9d66b8db80627
Peter Huang <peterhuang1113@gmail.com>
2024-12-24 23:21:29 -0500

    a

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
   ],
   use: {
     actionTimeout: 0,
-    baseURL: 'http://localhost:4200',
+    baseURL: 'http://localhost:3000',
 
     trace: 'on',
     video: 'on',
