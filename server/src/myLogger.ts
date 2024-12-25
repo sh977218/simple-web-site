10cd01488b61f17886f333fd498bba8e397b61f6
Peter Huang <peterhuang1113@gmail.com>
2024-12-24 19:24:06 -0500

    feat(repo): add winston logger

--- server/src/myLogger.ts
+++ server/src/myLogger.ts
@@ -19,42 +19,42 @@ export class MyLogger implements LoggerService {
   /**
    * Write a 'log' level log.
    */
-  log(message: any, ...optionalParams: any[]) {
+  log(message: string) {
     this.logger.log({ level: 'info', message });
   }
 
   /**
    * Write a 'fatal' level log.
    */
-  fatal(message: any, ...optionalParams: any[]) {
+  fatal(message: string) {
     this.logger.log({ level: 'fatal', message });
   }
 
   /**
    * Write an 'error' level log.
    */
-  error(message: any, ...optionalParams: any[]) {
+  error(message: string) {
     this.logger.log({ level: 'error', message });
   }
 
   /**
    * Write a 'warn' level log.
    */
-  warn(message: any, ...optionalParams: any[]) {
+  warn(message: string) {
     this.logger.log({ level: 'warn', message });
   }
 
   /**
    * Write a 'debug' level log.
    */
-  debug?(message: any, ...optionalParams: any[]) {
+  debug?(message: string) {
     this.logger.log({ level: 'debug', message });
   }
 
   /**
    * Write a 'verbose' level log.
    */
-  verbose?(message: any, ...optionalParams: any[]) {
+  verbose?(message: string) {
     this.logger.log({ level: 'verbose', message });
   }
 }
