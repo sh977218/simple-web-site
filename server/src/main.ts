b2f29fded9ce96ed5aa0967d481449efc162b650
Peter Huang <peterhuang1113@gmail.com>
2024-12-24 19:23:13 -0500

    a

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import cookieParser from 'cookie-parser';
-import { Logger } from 'src/logger';
 import { AppModule } from 'src/app.module';
+import { MyLogger } from 'src/myLogger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: new MyLogger(),
+  });
   app.enableCors();
   //  app.use(cookieParser());
   await app.listen(process.env['PORT'] ?? 3000);
