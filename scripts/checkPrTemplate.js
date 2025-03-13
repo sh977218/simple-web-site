2d4a8c5d6b715fb5deb131f06630ae00fcd40d7d
Peter Huang <peterhuang1113@gmail.com>
2025-03-13 07:38:50 -0400

    fix script

--- scripts/checkPrTemplate.js
+++ scripts/checkPrTemplate.js
@@ -3,11 +3,11 @@ checkPrTemplate(prBody);
 
 function checkString(s) {
   console.log(
-    '------------------------------------------------------------------'
+    '------------------------------------------------------------------',
   );
   console.log('Input string: '.concat(s));
   console.log(
-    '------------------------------------------------------------------'
+    '------------------------------------------------------------------',
   );
   let checkListRegex = new RegExp(/(- \[.+\].+)/g);
   let checkList = s.match(checkListRegex);
@@ -30,7 +30,7 @@ function checkString(s) {
   if (uncompletedTasks.length) {
     console.log('uncompletedTasks: \n'.concat(uncompletedTasks.join('\n')));
     console.log(
-      '------------------------------------------------------------------'
+      '------------------------------------------------------------------',
     );
     process.exit(1);
   } else {
@@ -48,7 +48,7 @@ function getPrBody() {
     process.exit(1);
   } else {
     let argvArray = argv.split('=');
-    if (argvArray.length != 2) {
+    if (argvArray.length !== 2) {
       process.exit(1);
     } else {
       return argvArray[1];
