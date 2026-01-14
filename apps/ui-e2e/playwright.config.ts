e76a0814c46e9a824876699d00e110f6efc80faf
Peter Huang <peterhuang1113@gmail.com>
2026-01-14 14:30:09 -0500

    try this

--- apps/ui-e2e/playwright.config.ts
+++ apps/ui-e2e/playwright.config.ts
@@ -54,8 +54,8 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: [
     {
-      command: 'npm run start:build',
-      url: 'http://localhost:3000',
+      command: 'npm run start:ui',
+      url: 'http://localhost:4200',
       reuseExistingServer: true,
       cwd: workspaceRoot,
     },
