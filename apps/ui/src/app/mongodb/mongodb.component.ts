a005112a594fcbcc945d36093efab5fd3fc6cf67
Peter Huang <peterhuang1113@gmail.com>
2026-01-15 11:46:43 -0500

    feat(ui): using http resources (#154)

--- apps/ui/src/app/mongodb/mongodb.component.ts
+++ apps/ui/src/app/mongodb/mongodb.component.ts
@@ -1,23 +1,22 @@
-import { Hero, HeroResponseSchema } from '../model/hero';
-import { verifyResponse } from '../verifyResponse';
-import { Component, inject } from '@angular/core';
-import { AsyncPipe } from '@angular/common';
+import { HeroResponseSchema } from '../model/hero';
+import { Component } from '@angular/core';
 import { HeroComponent } from '../hero/hero.component';
 import { MatDivider } from '@angular/material/list';
-import { catchError } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { httpResource } from '@angular/common/http';
 
 @Component({
   templateUrl: './mongodb.component.html',
-  imports: [AsyncPipe, HeroComponent, MatDivider],
+  imports: [HeroComponent, MatDivider],
 })
 export class MongodbComponent {
-  private http = inject(HttpClient);
+  private url = 'http://localhost:3000/api/heroes/100';
 
-  heroesFromMongo$ = this.http
-    .get<Hero[]>('http://localhost:3000/api/heroes/100')
-    .pipe(
-      verifyResponse(HeroResponseSchema),
-      catchError(() => []),
-    );
+  heroes = httpResource(
+    () => ({
+      url: this.url,
+    }),
+    {
+      parse: HeroResponseSchema.parse,
+    },
+  );
 }
