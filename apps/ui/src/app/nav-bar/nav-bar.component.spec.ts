cf332aa90b2cb6836f935089b1c1a3a6f686e360
Peter Huang <peterhuang1113@gmail.com>
2026-01-17 23:23:34 -0500

    fix unit tests

--- apps/ui/src/app/nav-bar/nav-bar.component.spec.ts
+++ apps/ui/src/app/nav-bar/nav-bar.component.spec.ts
@@ -1,28 +1,32 @@
-import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavBarComponent } from './nav-bar.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { provideRouter } from '@angular/router';
 
 describe('NavBarComponent', () => {
   let component: NavBarComponent;
   let fixture: ComponentFixture<NavBarComponent>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [NavBarComponent],
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
       imports: [
+        NavBarComponent,
         MatButtonModule,
         MatIconModule,
         MatListModule,
         MatSidenavModule,
         MatToolbarModule,
-      ]
-    });
-  }));
+      ],
+      providers: [provideRouter([])],
+    }).compileComponents();
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(NavBarComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should compile', () => {
