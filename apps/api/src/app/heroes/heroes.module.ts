761db462ab6d5f2206f485d38d5a7e3a690bfec9
Peter Huang <peterhuang1113@gmail.com>
2026-01-19 13:58:38 -0500

    fix(api): fix schema error (#185)

--- apps/api/src/app/heroes/heroes.module.ts
+++ apps/api/src/app/heroes/heroes.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
-import { HeroSchema } from '@shared/shared-models';
-
 import { HeroesController } from './heroes.controller';
 import { HeroesService } from './heroes.service';
-import { Hero } from './schemas/hero.schema'
+import { Hero, HeroSchema } from './schemas/hero.schema';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: Hero.name, schema: HeroSchema }])],
