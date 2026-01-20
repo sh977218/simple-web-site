733e0a1166517eafe8f301f6688492c23f2d230e
Peter Huang <peterhuang1113@gmail.com>
2026-01-20 14:54:19 -0500

    fix(ui): try to fix ssr client render for router

--- apps/ui/src/app/app.routes.server.ts
+++ apps/ui/src/app/app.routes.server.ts
@@ -2,7 +2,19 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    path: 'mongodb',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'elasticsearch',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'threeJs',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'excel',
+    renderMode: RenderMode.Prerender,
+  },
 ];
