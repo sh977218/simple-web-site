4577f9bbb2a058c7d5233067a09a3647c7ce1500
Peter Huang <peterhuang1113@gmail.com>
2026-01-20 16:23:28 -0500

    Revert "fix(ui): try to fix ssr client render for router (#191)"
    This reverts commit a5b30bde64e2108ae5980fc50b826bd069dcbc91.

--- apps/ui/src/app/app.routes.server.ts
+++ apps/ui/src/app/app.routes.server.ts
@@ -2,19 +2,7 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'mongodb',
-    renderMode: RenderMode.Client,
-  },
-  {
-    path: 'elasticsearch',
-    renderMode: RenderMode.Prerender,
-  },
-  {
-    path: 'threeJs',
-    renderMode: RenderMode.Prerender,
-  },
-  {
-    path: 'excel',
-    renderMode: RenderMode.Prerender,
-  },
+    path: '**',
+    renderMode: RenderMode.Prerender
+  }
 ];
