f61690da82de65091d04dc78f5b431431a8a468c
Peter Huang <peterhuang1113@gmail.com>
2026-01-22 14:40:46 -0500

    feat(ui): add mat search bar (#198)

--- apps/ui/src/app/mongodb/mongodb.component.ts
+++ apps/ui/src/app/mongodb/mongodb.component.ts
@@ -1,18 +1,33 @@
 import { HeroZod } from '@shared/shared-models';
-import { Component, effect, inject } from '@angular/core';
+import { Component, effect, inject, model } from '@angular/core';
 import { HeroComponent } from '../hero/hero.component';
 import { MatDivider } from '@angular/material/list';
 import { httpResource } from '@angular/common/http';
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { environment } from '../../environments/environment';
+import { MatFormField, MatInput, MatSuffix } from '@angular/material/input';
+import { MatIconButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   templateUrl: './mongodb.component.html',
-  imports: [HeroComponent, MatDivider, MatProgressSpinner],
+  imports: [
+    HeroComponent,
+    MatDivider,
+    MatProgressSpinner,
+    MatFormField,
+    MatInput,
+    MatIconButton,
+    MatIcon,
+    FormsModule,
+    MatSuffix
+  ],
 })
 export class MongodbComponent {
   private _snackBar = inject(MatSnackBar);
+  searchTerm = model<string>();
 
   private url = `${environment.api}/heroes`;
 
