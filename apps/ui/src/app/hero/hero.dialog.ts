e370741f154b5e3c4dd762aed828ae3839e83f80
Peter Huang <peterhuang1113@gmail.com>
2026-01-22 22:14:18 -0500

    refactor(ui): separate hero and member (#202)

---
+++ apps/ui/src/app/hero/hero.dialog.ts
@@ -0,0 +1,51 @@
+import { Component, inject } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+
+import { Hero } from '@shared/shared-models';
+import { MatButton } from '@angular/material/button';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+
+@Component({
+  imports: [
+    MatCardModule,
+    MatListModule,
+    MatButton,
+    MatDialogClose,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+  ],
+  template: `
+    <h2 mat-dialog-title>{{ hero.squadName }}</h2>
+    <mat-dialog-content>
+      <mat-list>
+        <mat-list-item>
+          <span matListItemTitle>home town</span>
+          <span matListItemLine>{{ hero.homeTown }}</span>
+        </mat-list-item>
+        <mat-list-item>
+          <span matListItemTitle>secret base</span>
+          <span matListItemLine>{{ hero.secretBase }}</span>
+        </mat-list-item>
+        <mat-list-item>
+          <span matListItemTitle>members</span>
+          <span matListItemLine>{{ hero.members.length }}</span>
+        </mat-list-item>
+      </mat-list>
+    </mat-dialog-content>
+    <mat-dialog-actions>
+      <button matButton mat-dialog-close cdkFocusInitial>Ok</button>
+    </mat-dialog-actions>
+  `,
+})
+export class HeroDialog {
+  readonly data = inject<{ hero: Hero }>(MAT_DIALOG_DATA);
+  readonly hero = this.data.hero;
+}
