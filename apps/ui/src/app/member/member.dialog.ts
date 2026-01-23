c1d2b47e81c813cf2e3ca78ef51b4fd52b03e9cd
Peter Huang <peterhuang1113@gmail.com>
2026-01-22 22:52:57 -0500

    fix(ui): member dialog (#204)

---
+++ apps/ui/src/app/member/member.dialog.ts
@@ -0,0 +1,54 @@
+import { Component, inject } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatChip, MatChipSet } from '@angular/material/chips';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import { MatListModule } from '@angular/material/list';
+import { Member } from '@shared/shared-models';
+
+@Component({
+  imports: [
+    MatCardModule,
+    MatListModule,
+    MatButton,
+    MatDialogClose,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatChip,
+    MatChipSet,
+  ],
+  template: `
+    <h2 mat-dialog-title>{{ member.name }}</h2>
+    <mat-dialog-content>
+      <mat-list>
+        <mat-list-item>
+          <span matListItemTitle>age</span>
+          <span matListItemLine>{{ member.age }}</span>
+        </mat-list-item>
+        <mat-list-item>
+          <span matListItemTitle>secret identity</span>
+          <span matListItemLine>{{ member.secretIdentity }}</span>
+        </mat-list-item>
+      </mat-list>
+      <mat-chip-set aria-label="member powers">
+        @for (power of member.powers; track power) {
+          <mat-chip>{{ power }}</mat-chip>
+        }
+      </mat-chip-set>
+    </mat-dialog-content>
+    <mat-dialog-actions>
+      <button matButton mat-dialog-close cdkFocusInitial>Ok</button>
+    </mat-dialog-actions>
+  `,
+})
+export class MemberDialog {
+  readonly data = inject<{ member: Member }>(MAT_DIALOG_DATA);
+  readonly member = this.data.member;
+}
