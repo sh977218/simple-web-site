31b704090a82415dfe18d64055137fd6fcb80928
Peter Huang <peterhuang1113@gmail.com>
2026-01-24 21:16:31 -0500

    fix(ui): fix search (#212)

--- apps/ui-e2e/src/tests/search-page.spec.ts
+++ apps/ui-e2e/src/tests/search-page.spec.ts
@@ -2,6 +2,14 @@ import { expect } from '@playwright/test';
 
 import { test } from '../fixtures/search.fixture';
 
-test('Search', async ({ searchPage }) => {
-  await expect(searchPage.searchResult).toBeVisible();
+test('Search with result', async ({ hasSearchResult }) => {
+  await hasSearchResult.searchInput.fill('hero')
+  await expect(hasSearchResult.searchResult.getByRole('listitem')).not.toHaveCount(0);
+});
+
+test('Search without result', async ({ hasNoSearchResults }) => {
+  await hasNoSearchResults.searchInput.fill('aaa');
+  await expect(
+    hasNoSearchResults.searchResult.getByRole('listitem'),
+  ).not.toHaveCount(0);
 });
--- apps/ui/src/app/search/search.facade.ts
+++ apps/ui/src/app/search/search.facade.ts
@@ -1,6 +1,6 @@
 import { httpResource } from '@angular/common/http';
 import { Injectable, signal } from '@angular/core';
-import { form, required } from '@angular/forms/signals';
+import { debounce, form, required } from '@angular/forms/signals';
 import { Hero } from '@shared/shared-models';
 
 import { environment } from '../../environments/environment';
@@ -9,7 +9,6 @@ import { SearchForm } from './search';
 
 @Injectable({ providedIn: 'root' })
 export class SearchFacade {
-  searchTerm = signal<string | undefined>(undefined);
   private url = `${environment.api}/heroes`;
 
   defaultSearchForm = signal<SearchForm>({
@@ -17,11 +16,12 @@ export class SearchFacade {
   });
 
   searchForm = form(this.defaultSearchForm, (schemaPath) => {
+    debounce(schemaPath.searchTerm, 500);
     required(schemaPath.searchTerm, { message: 'searchTerm is required' });
   });
 
   searchedHeroes = httpResource<Hero[]>(() => {
-    const searchTerm = this.searchTerm();
+    const searchTerm = this.searchForm.searchTerm().value();
     if (searchTerm) {
       return {
         url: this.url,
@@ -33,4 +33,12 @@ export class SearchFacade {
       url: this.url,
     };
   });
+
+  search() {
+    /* empty */
+  }
+
+  onReset() {
+    this.searchForm().reset();
+  }
 }
