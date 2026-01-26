bf8a24beb4772ac405a7687ee6368057dc0f4518
Peter Huang <peterhuang1113@gmail.com>
2026-01-25 21:29:28 -0500

    fix(ui): angualr vitest playwright browser (#215)

--- apps/ui/vitest.config.ts
+++ apps/ui/vitest.config.ts
@@ -1,16 +1,13 @@
 import { playwright } from '@vitest/browser-playwright';
 import { defineConfig } from 'vitest/config';
 
-const isCI = !!process.env.CI;
-
 export default defineConfig({
   test: {
     browser: {
       provider: playwright({
-        // ...custom playwright options
       }),
-      enabled: !isCI,
-      instances: [{ browser: 'chromium' }],
+      enabled: true,
+      instances: [{ browser: 'chromium' }]
     },
-  },
+  }
 });
