d43c3891f787fc0ca30af4c0ac16b104fe324fe6
Peter Huang <peterhuang1113@gmail.com>
2026-02-10 16:20:44 -0500

    feat(ui): introduce highcharts

--- apps/ui/src/app/app.config.ts
+++ apps/ui/src/app/app.config.ts
@@ -13,6 +13,7 @@ import {
   withComponentInputBinding,
 } from '@angular/router';
 import { provideNgtRenderer } from 'angular-three/dom';
+import { provideHighcharts } from 'highcharts-angular';
 
 import { appRoutes } from './app.routes';
 
@@ -36,6 +37,30 @@ export const appConfig: ApplicationConfig = {
     provideZonelessChangeDetection(),
     provideRouter(appRoutes, withComponentInputBinding()),
     provideNgtRenderer(),
+    provideHighcharts({
+      instance: () => import('highcharts'),
+
+      // Global chart options applied across all charts
+      options: {
+        title: {
+          style: {
+            color: 'tomato',
+          },
+        },
+        legend: {
+          enabled: false,
+        },
+      },
+
+      // Include Highcharts additional modules (e.g., exporting, accessibility) or custom themes
+      modules: () => {
+        return [
+          import('highcharts/esm/modules/accessibility'),
+          import('highcharts/esm/modules/exporting'),
+          import('highcharts/esm/themes/sunset'),
+        ];
+      },
+    }),
     { provide: TitleStrategy, useClass: TemplatePageTitleStrategy },
   ],
 };
--- apps/ui/src/app/dashboard/dashboard.component.ts
+++ apps/ui/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ChartConstructorType, HighchartsChartComponent, HighchartsChartDirective } from 'highcharts-angular';
 
 import { MaterialModule } from '../material.module';
 
@@ -12,21 +13,45 @@ interface Car {
 
 @Component({
   templateUrl: './dashboard.component.html',
-  imports: [MaterialModule, ReactiveFormsModule, ExcelComponent]
+  styles: [
+    `
+      .chart {
+        width: 100%;
+        height: 400px;
+        display: block;
+      }
+    `,
+  ],
+  imports: [
+    MaterialModule,
+    ReactiveFormsModule,
+    ExcelComponent,
+    HighchartsChartComponent,
+  ],
 })
 export class DashboardComponent {
   private _formBuilder = inject(FormBuilder);
 
   firstFormGroup = this._formBuilder.group({
-    firstCtrl: ['', Validators.required]
+    firstCtrl: ['', Validators.required],
   });
   secondFormGroup = this._formBuilder.group({
-    secondCtrl: ['', Validators.required]
+    secondCtrl: ['', Validators.required],
   });
 
   cars: Car[] = [
     { value: 'volvo', viewValue: 'Volvo' },
     { value: 'saab', viewValue: 'Saab' },
-    { value: 'mercedes', viewValue: 'Mercedes' }
+    { value: 'mercedes', viewValue: 'Mercedes' },
   ];
+
+  chartOptions: Highcharts.Options = {
+    series: [
+      {
+        data: [1, 2, 3],
+        type: 'line',
+      },
+    ],
+  };
+  chartConstructor: ChartConstructorType = 'chart';
 }
