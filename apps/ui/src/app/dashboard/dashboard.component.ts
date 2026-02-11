b54a4c51d3781db0759bfde0e0d4b3311a162d4b
Peter Huang <peterhuang1113@gmail.com>
2026-02-10 21:00:13 -0500

    fix(ui): fix stepper (#299)

--- apps/ui/src/app/dashboard/dashboard.component.ts
+++ apps/ui/src/app/dashboard/dashboard.component.ts
@@ -6,11 +6,6 @@ import { MaterialModule } from '../material.module';
 import { ExcelComponent } from './excel.component';
 import { ExcelService } from './excel.service';
 
-interface Car {
-  value: string;
-  viewValue: string;
-}
-
 @Component({
   templateUrl: './dashboard.component.html',
   providers: [ExcelService],
@@ -23,13 +18,7 @@ export class DashboardComponent {
     firstCtrl: ['', Validators.required],
   });
   secondFormGroup = this._formBuilder.group({
-    secondCtrl: ['', Validators.required],
+    columnCtrl: ['', Validators.required],
     typeCtrl: ['', Validators.required],
   });
-
-  cars: Car[] = [
-    { value: 'volvo', viewValue: 'Volvo' },
-    { value: 'saab', viewValue: 'Saab' },
-    { value: 'mercedes', viewValue: 'Mercedes' },
-  ];
 }
--- apps/ui/src/app/dashboard/excel.service.ts
+++ apps/ui/src/app/dashboard/excel.service.ts
@@ -1,5 +1,8 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 
 @Injectable()
 export class ExcelService {
+  fileName = signal('');
+  rowData = signal<Record<string, unknown>[]>([]);
+  headers = signal<string[]>([]);
 }
